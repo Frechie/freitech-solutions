0129032463e32ceee19ed36d3fb9e88b2dc38437
Frechie <frechyboi@gmail.com>
2021-03-02 10:38:27 +0100

    Edited CSS

--- resources/views/services/web-design.blade.php
+++ resources/views/services/web-design.blade.php
@@ -11,8 +11,40 @@
             </div>
 
             <div class="col-lg-3 col-md-5">
-                <div class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                <img class="img-fluid" src="{{asset('assets/img/slide/slide-1.jpg') }}" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-7">
+                <article style="text-align: justify;">
+                    <h2>Web Design &amp; Development</h2>
+
+                    <p>
+                        This introduction to Python will kickstart your learning of&nbsp;<strong>Python&nbsp;
+                        for data science</strong>, as well as programming in general. This beginner-friendly 
+                        Python course will take you from zero to programming in Python in a matter of hours.
+                    </p>
+
+                    <p>
+                        Upon its completion, you&#39;ll be able to write your own Python scripts and perform 
+                        basic hands-on data analysis using our Jupyter-based lab environment. If you want to 
+                        learn Python from scratch, this free course is for you.
+                    </p>
+
+                    <p>
+                        You can start creating your own data science projects and collaborating with other 
+                        data scientists using&nbsp;<a href="http://cocl.us/PythonforDataScienceMainPage" rel="noopener" target="_blank">IBM Watson Studio</a>. When you sign up, you get free access to Watson Studio. Start now and take advantage of this platform.</p>
+
+                    <hr />
+                </article>
+            </div>
+            <div class="col-lg-4 col-md-5">
+                <div class="list-group rounded">
+                    <a href="#" class="list-group-item list-group-item-action btn-explore" aria-current="true">
                         The current link item
                     </a>
                     <a href="#" class="list-group-item list-group-item-action">A second link item</a>
@@ -24,6 +56,7 @@
         </div>
     </div>
 </section>
+
 
 <section>
 </section>
